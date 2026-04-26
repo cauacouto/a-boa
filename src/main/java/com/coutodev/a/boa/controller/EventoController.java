90116c1155fe39cc29df601008200f4e4c1e5444
cauacouto <cauacouto022@gmail.com>
2026-04-25 23:19:32 -0300

    update: add eventoResponseDto

--- src/main/java/com/coutodev/a/boa/controller/EventoController.java
+++ src/main/java/com/coutodev/a/boa/controller/EventoController.java
@@ -1,39 +1,42 @@
 package com.coutodev.a.boa.controller;
 
 import com.coutodev.a.boa.DTO.AtualizaçaoEvento;
-import com.coutodev.a.boa.DTO.EventoRequest;
-import com.coutodev.a.boa.DTO.EventoResponse;
+import com.coutodev.a.boa.DTO.EventoResponseDto;
+import com.coutodev.a.boa.DTO.EventoRequestDto;
 import com.coutodev.a.boa.Service.EventoService;
+import com.coutodev.a.boa.domin.Evento;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/evento")
 public class EventoController {
 
 
     @Autowired
     EventoService service;
 
-    @PostMapping("/criarEvento")
-    public ResponseEntity criarEvento(@RequestBody @Valid EventoRequest dto) {
+    @PostMapping()
+    public ResponseEntity<EventoResponseDto> criarEvento(@RequestBody @Valid EventoRequestDto dto) {
         this.service.criarEvento(dto);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
-    public ResponseEntity atualizar(@RequestBody @Valid AtualizaçaoEvento dto) {
+    public ResponseEntity<Void> atualizar(@RequestBody @Valid AtualizaçaoEvento dto) {
         return this.service.atualizarEvento(dto)
                 ? ResponseEntity.ok().build()
                 : ResponseEntity.notFound().build();
     }
     @GetMapping
     public ResponseEntity listarEventos() {
-        List<EventoResponse> eventos = this.service.listarEventos();
+        List<EventoRequestDto> eventos = this.service.listarEventos();
         return ResponseEntity.ok(eventos);
     }
     @DeleteMapping("/{id}")
@@ -41,6 +44,11 @@ public class EventoController {
     public ResponseEntity deletar(@PathVariable Long id) {
         service.deletarEvento(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/imagem")
+    public ResponseEntity<Evento> uploadImagem(@PathVariable Long id,@RequestParam("file") MultipartFile file){
+        return ResponseEntity.ok(service.uploadImagem(id,file));
     }
 
 
