37298afde0808d50c2a511fe87992fb48bd684a8
cauacouto <cauacouto022@gmail.com>
2026-04-25 23:17:44 -0300

    feat: criar metodo uploadsImagem, update:eventoResponseDto metodo criarEvento

--- src/main/java/com/coutodev/a/boa/Service/EventoService.java
+++ src/main/java/com/coutodev/a/boa/Service/EventoService.java
@@ -1,28 +1,41 @@
 package com.coutodev.a.boa.Service;
 
 import com.coutodev.a.boa.DTO.AtualizaçaoEvento;
-import com.coutodev.a.boa.DTO.EventoRequest;
-import com.coutodev.a.boa.DTO.EventoResponse;
+import com.coutodev.a.boa.DTO.EventoRequestDto;
+import com.coutodev.a.boa.DTO.EventoResponseDto;
 import com.coutodev.a.boa.Repository.EventoRpository;
-import com.coutodev.a.boa.domin.evento;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.coutodev.a.boa.domin.Evento;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EventoService {
 
-    @Autowired
-    private EventoRpository eventoRepository;
 
-    public EventoService(EventoRpository eventoRepository) {
+    private final EventoRpository eventoRepository;
+
+    private final String uploadDir = "uploads/eventos/";
+    private final List<String> tiposPermitidos = List.of("image/jpeg","image/png","image/webp");
+
+    private final ModelMapper mapper;
+
+    public EventoService(EventoRpository eventoRepository, ModelMapper mapper) {
         this.eventoRepository = eventoRepository;
+        this.mapper = mapper;
     }
 
-    public void criarEvento(EventoRequest dto) {
-        evento novoEvento = new evento(dto);
-        this.eventoRepository.save(novoEvento);
+    public EventoResponseDto criarEvento(EventoRequestDto dto) {
+        Evento evento = mapper.map(dto, Evento.class);
+        evento = eventoRepository.save(evento);
+        return mapper.map(evento,EventoResponseDto.class);
     }
 
     public boolean atualizarEvento(AtualizaçaoEvento dto) {
@@ -35,10 +48,10 @@ public class EventoService {
         return true;
     }
 
-    public List<EventoResponse> listarEventos() {
+    public List<EventoRequestDto> listarEventos() {
         return eventoRepository.findAll()
                 .stream()
-                .map(EventoResponse::new)
+                .map(EventoRequestDto::new)
                 .toList();
     }
 
@@ -46,10 +59,31 @@ public class EventoService {
         eventoRepository.deleteById(id);
     }
 
-    public EventoResponse buscarPorId(Long id) {
+    public EventoRequestDto buscarPorId(Long id) {
         return eventoRepository.findById(id)
-                .map(EventoResponse::new)
+                .map(EventoRequestDto::new)
                 .orElse(null);
     }
+
+    public Evento uploadImagem(Long id, MultipartFile file){
+        if (!tiposPermitidos.contains(file.getContentType())){
+            throw new RuntimeException("apenas imagens são permitidas");
+        }
+        Evento evento = eventoRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("evento não encontrado"));
+
+        try {
+            String nomeArquivo = id + "-" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+            Path path = Paths.get(uploadDir + nomeArquivo);
+            Files.createDirectories(path.getParent());
+            Files.write(path,file.getBytes());
+
+            evento.setImageUrl("uploads/eventos/" + nomeArquivo);
+            return eventoRepository.save(evento);
+        }catch (IOException e){
+            throw  new RuntimeException("erro ao salvar imagem ", e);
+        }
+    }
+
 }
 
