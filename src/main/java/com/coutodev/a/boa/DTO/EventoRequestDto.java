bf91ca5714d65c7633bc1aa499066a7f2633343c
cauacouto <cauacouto022@gmail.com>
2026-04-28 08:04:37 -0300

    update: trocar record para class

---
+++ src/main/java/com/coutodev/a/boa/DTO/EventoRequestDto.java
@@ -0,0 +1,19 @@
+package com.coutodev.a.boa.DTO;
+
+import com.coutodev.a.boa.domin.TipoDoEvento;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventoRequestDto {
+    private String nome;
+    private TipoDoEvento tipo;
+    private LocalDateTime data;
+    private String local;
+    private String descricao;
+    private String organizador;
+}
