4f71f9b741b041bb8a1c7aa0feaafa88254deb96
cauacouto <cauacouto022@gmail.com>
2026-04-28 08:04:17 -0300

    update: retira getter e settes manual

--- src/main/java/com/coutodev/a/boa/DTO/EventoResponseDto.java
+++ src/main/java/com/coutodev/a/boa/DTO/EventoResponseDto.java
@@ -1,68 +1,26 @@
 package com.coutodev.a.boa.DTO;
 
 import com.coutodev.a.boa.domin.TipoDoEvento;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventoResponseDto {
-      private String nome;
-      private TipoDoEvento tipo;
-      private LocalDateTime data;
-
-      private String local;
-     private String descrição;
-      private String organizador;
-
-      public EventoResponseDto(){}
 
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public TipoDoEvento getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoDoEvento tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDateTime getData() {
-        return data;
-    }
-
-    public void setData(LocalDateTime data) {
-        this.data = data;
-    }
+    private Long id;
+    private String nome;
+    private TipoDoEvento tipo;
+    private LocalDateTime data;
+    private String local;
+    private String descricao;
+    private String organizador;
 
 
-
-    public String getLocal() {
-        return local;
-    }
-
-    public void setLocal(String local) {
-        this.local = local;
-    }
-
-    public String getDescrição() {
-        return descrição;
-    }
-
-    public void setDescrição(String descrição) {
-        this.descrição = descrição;
-    }
-
-    public String getOrganizador() {
-        return organizador;
-    }
-
-    public void setOrganizador(String organizador) {
-        this.organizador = organizador;
-    }
 }
+
+
