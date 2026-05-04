03d7e9168eaf355ebc3a32ccc6f78432def58c68
cauacouto <cauacouto022@gmail.com>
2026-05-03 21:23:16 -0300

    feat:criar UsuarioService

---
+++ src/main/java/com/coutodev/a/boa/Service/UsuarioService.java
@@ -0,0 +1,19 @@
+package com.coutodev.a.boa.Service;
+
+import com.coutodev.a.boa.DTO.DadosCadastroDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+
+
+    public void registrar(DadosCadastroDto dados){
+
+    }
+
+
+
+}
