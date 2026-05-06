85b70bba4f6a8529dab9d0d79dcc621cb670fb9f
cauacouto <cauacouto022@gmail.com>
2026-05-06 19:05:05 -0300

    feat: update user registration and login process, add CORS configuration, and enhance event handling

--- src/main/java/com/coutodev/a/boa/controller/AuthController.java
+++ src/main/java/com/coutodev/a/boa/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.coutodev.a.boa.controller;
 
 import com.coutodev.a.boa.DTO.DadosCadastroDto;
+import com.coutodev.a.boa.DTO.DadosCadastroResponse;
+import com.coutodev.a.boa.DTO.DadosLoginDto;
 import com.coutodev.a.boa.DTO.DadosTokenJWT;
 import com.coutodev.a.boa.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +22,13 @@ public class AuthController {
     private UsuarioService usuarioService;
 
 
-    @PostMapping
-    public ResponseEntity<Void> register(@RequestBody DadosCadastroDto dados ){
-        usuarioService.registrar(dados);
-        return ResponseEntity.ok().build();
+    @PostMapping("/register")
+    public ResponseEntity<DadosCadastroResponse> register(@RequestBody DadosCadastroDto dados ){
+       DadosCadastroResponse response = usuarioService.registrar(dados);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
-    @PostMapping
-    public ResponseEntity login(@RequestBody DadosCadastroDto dto){
+    @PostMapping("/login")
+    public ResponseEntity<DadosTokenJWT> login(@RequestBody DadosLoginDto dto){
         var token = usuarioService.login(dto);
         return ResponseEntity.ok(new DadosTokenJWT(token));
     }
